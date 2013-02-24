af451d93c474a228ebcf3a38ec89622b3525ec8e
Rodrigo Pavano <rodrigopavano@gmail.com>
2013-02-24 16:43:09 -0300

    added git alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,6 +11,7 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias ga='git add'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
@@ -18,3 +19,4 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
