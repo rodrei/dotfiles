e7a0256920b22563c94259be7efe94cbf6a062d6
rodrei <rodrigopavano@gmail.com>
2014-07-02 20:31:05 -0300

    Added comment to enable mac ports

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,4 @@
+#Uncomment for MacPorts
+#export PATH="/opt/local/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
