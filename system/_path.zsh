6b3a250a6d2b483cdce968a2d6c123f733d1b2a8
Rodrigo Pavano <rodrigopavano@gmail.com>
2014-07-16 10:16:14 -0300

    Adding rvm to path

--- system/_path.zsh
+++ system/_path.zsh
@@ -2,3 +2,6 @@
 #export PATH="/opt/local/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+#RVM
+export PATH="$HOME/.rvm/bin:$PATH" # Add RVM to PATH for scripting
